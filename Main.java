4a308567cb543fa29646b461149b0752ecaad097
DongHyun Yang <dh.yang@alcherainc.com>
2023-08-31 15:58:34 +0900

    [KBANK] 키 관리를 직접하지 않고 키를 난독화하여 라이브러리가 관리하도록 수정 - AS-IS: key와 iv를 생성자에 넣거나 env로 셋팅하는 경우만 동작 - TO-BE: key와 iv를 생성자에 넣거나 env로 셋팅하지 않는 경우, 난독화된 key와 iv로를 난독화를 풀어서 key와 iv를 가져오고 해당 값으로 암호화
    - Main.java (샘플코드 추가)
    - README.md (사용법 업데이트 : 변경사항반영 & 샘플코드 빌드 및 실행법 추가)

---
+++ Main.java
@@ -0,0 +1,56 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import kr.co.useb.AES256;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        AES256 aes = new AES256();
+
+        // 평문 string값을 암호화하는 예시
+        String src = "{\"idType\":\"2\",\"userName\":\"홍길동\",\"driverNo\":\"13-03-123456-11\",\"juminNo1\":\"910101\",\"juminNo2\":\"1001234\",\"_juminNo2\":\"1******\",\"issueDate\":\"20170131\",\"transaction_id\":\"14740463576391927d859b91670484605\"}";
+
+        String encrypted = aes.encrypt(src);
+        System.out.println("encrypted : ");
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println(encrypted);
+        System.out.println("==========================================================================================");
+
+        // 암호화된 string값을 복호화하는 예시
+        String decrypted = aes.decrypt(encrypted);
+        System.out.println("decrypted : ");
+        System.out.println("------------------------------------------------------------------------------------------");
+        System.out.println(decrypted);
+        System.out.println("==========================================================================================");
+
+        Path of = Path.of("");
+        try {
+            // 암호화된 text 파일(을 읽어서 복호화 하는 예시
+            String currentPath = of.toAbsolutePath().toString();
+            String filePath = Paths.get(currentPath, "testset", "encrypted.txt").toString();
+            String decryptedFromFile = aes.decryptFromFile(filePath);
+            System.out.println("decrypted from file : ");
+            System.out.println("------------------------------------------------------------------------------------------");
+            System.out.println(decryptedFromFile);
+            System.out.println("==========================================================================================");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+
+        try {
+            // 평문 text 파일을 읽어서 암호화 하는 예시
+            String currentPath = of.toAbsolutePath().toString();
+            String filePath = Paths.get(currentPath, "testset", "plainData.txt").toString();
+            String encryptedFromFile = aes.encryptFromFile(filePath);
+            System.out.println("encrypted from file : ");
+            System.out.println("------------------------------------------------------------------------------------------");
+            System.out.println(encryptedFromFile);
+            System.out.println("==========================================================================================");
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,12 +1,40 @@
 ## AES256.jar Library
-AES256 모듈을 사용하기 위해서는 aes_key값과 aes_iv값이 필요합니다.
-aes_key, aes_iv 입력 방법은 2가지 입니다.
 
-1. 생성자로 전달
-```java
-new AES256("aes_key", "aes_iv");
+### 샘플코드의 빌드 및 실행
+
+#### 빌드명령어 
+```bash
+$ javac -cp libs/AES256.jar -encoding UTF-8 Main.java
 ```
-2. 환경 변수로 전달
+
+#### 실행명령어
+리눅스 / Mac
+```bash
+java -cp .:libs/AES256.jar Main
+```
+
+윈도우
+```bash
+java -cp .;libs/AES256.jar Main
+```
+ 
+
+### 라이브러리 사용법
+
+#### 암호화 key / iv 설정 관련 설명
+AES256 모듈을 사용하기 위해서는 aes_key값과 aes_iv값이 필요합니다.
+aes_key, aes_iv 입력 방법은 3가지 입니다.
+
+1. 사용하는 곳에서는 별도 키관리를 하지 않고 lib가 난독화하여 관리하고 있는 암호화키를 그대로 사용하는 경우
+```java
+/* 
+ AES256 객체 생성하기  전에 환경변수에 aes_key, aes_iv 값을 셋팅하지 않거나 ""(빈값)으로 셋팅한 상태에서, 제목 없는 문서
+ default 생성자로 객체를 생성하여서 사용하시면 됩니다.
+*/
+new AES256();
+```
+
+2. key와 iv를 환경 변수로 전달하여 사용하는 방법
 ```java
 /* 
  AES256 객체 생성하기 전에 환경변수에 aes_key, aes_iv 값을 셋팅하시고 제목 없는 문서
@@ -15,11 +43,11 @@ new AES256("aes_key", "aes_iv");
 new AES256();
 ```
 
-aes_key와 aes_iv 값이 제대로 입력되지 않은 상태에서 관련 method 사용시 예외발생
+3. key와 iv를 생성자로 인자로 전달하여 사용하는 방법
 ```java
-java.lang.Exception: To use encryption, you need the AES key and AES IV values. 
-You can either populate values in the environment variables 'aes_key' and 'aes_iv' or utilize the AES256 constructor parameters.
+new AES256("aes_key", "aes_iv");
 ```
+
 
 ### Method 사용법
 #### encrypt
